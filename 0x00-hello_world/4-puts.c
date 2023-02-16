b0b20e5557f42f66b4b775f04a299addf7669739
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-16 07:46:54 -0800

    Writing first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Use puts function to print a line
+ * return: 0
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
