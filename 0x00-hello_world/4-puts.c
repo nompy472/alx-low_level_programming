9a8ab9b8fc30b8660b2ac35b92ce95f961f65685
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-16 08:02:03 -0800

    My first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Use puts function to print a line
- * return: 0
+ * main - Enrty point |Use puts function to print a line
+ * Return: 0
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+return (0);
 }
