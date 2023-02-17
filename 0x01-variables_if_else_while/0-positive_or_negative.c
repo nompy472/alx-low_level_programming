9509ef034b1008e36424b1bfb0965103dd989939
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-17 07:36:00 -0800

    Print negative or positive for variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  *main - Assign random numbers to variable n
- *Return: - n is positive or negative
+ *Return: 0
  */
+
 int main(void)
 {
-  int n;
-  if (n > 0)
-    {
-      printf("$n is positive\n");
-    }
-  else if (n < 0)
-    {
-      printf("$n is negative\n");
-    }
-  else
-    {
-      printf("$n is zero\n");
-    }
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	  {
+	    printf("%d is positive\n", n);
+	  }
+	else if (n < 0)
+	  {
+	    printf("%d is negative\n", n);
+	  }
+	else
+	  {
+	    printf("%d is zero\n", n);
+	  }
+	return (0);
 }
