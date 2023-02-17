d7be3d9c6fc78bb6af8e057b0390179488ed021a
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-17 04:48:57 -0800

    IF statements

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Assign random numbers to variable n
+ *Return: - n is positive or negative
+ */
+int main(void)
+{
+  int n;
+  if (n > 0)
+    {
+      printf("$n is positive\n");
+    }
+  else if (n < 0)
+    {
+      printf("$n is negative\n");
+    }
+  else
+    {
+      printf("$n is zero\n");
+    }
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+ C Variable and Conditional statements
