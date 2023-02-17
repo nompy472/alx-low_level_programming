f377e4b35f258f4aab89901d4db98878d09d78ee
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-17 12:21:33 -0800

    Change character

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Print alphabet in lower case
+ *Return: 0 (always)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= z)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
