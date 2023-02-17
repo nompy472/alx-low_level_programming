5ad3067b0b03b2184723b9037c26bf2fa08157a2
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-17 12:49:42 -0800

    Print letters with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Print in lowercase
+ *Return: 0
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+if ((c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
