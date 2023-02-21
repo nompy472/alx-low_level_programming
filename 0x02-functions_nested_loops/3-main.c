38261d67ab2a9c4b4217d124d9ddf334dc7442db
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-21 13:29:16 -0800

    Checks lowercases

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - check code
+ *Return:0
+*/
+int main(void)
+{
+int t;
+t = _islower('H');
+_putchar(t + '0');
+t = _islower('o');
+_putchar(t + '0');
+t = _islower(108);
+_putchar(t + '0');
+_putchar('\n');
+return (0);
+}
