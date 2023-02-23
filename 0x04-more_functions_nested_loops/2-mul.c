eeb0e83205a08337ef10784dd756397d31b2237a
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-02-23 03:29:44 -0800

    Product of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplication of integers
+ * @x: first integer
+ * @y: second integer
+ * Return: product of x and y
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
