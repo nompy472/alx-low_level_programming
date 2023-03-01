8688e5bc67d171bacaad209f929a4eb53c39e88d
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-03-01 10:15:50 -0800

    Copying string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates the string pointed to by @src, including the x
- *    null byte, to the end of the string pointed to by @dest
- *    @dest: A pointer to the string to be concatenated upon
- *    @src: The source string to be appended to @dest
- *     Return: A pointer to the destination string @dest
+ * _strncat - Concatenates two strings using at most
+ * an inputted number of bytes from src
+ * @dest: The string to be appended upon
+ * @src: The string to be appended to dest
+ * @n: The number of bytes from src to be appended to dest
+ * Return: A pointer to the resulting string dest
  */
 
-char *_strcat(char *dest, const char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
@@ -16,9 +17,9 @@ char *_strcat(char *dest, const char *src)
 	{
 		dest_len++;
 	}
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 	{
-		dest[dest_len++] = src[index]
+		dest[dest_len++] = src[index];
 	}
 	return (dest);
 }
