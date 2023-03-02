8e8f1151d1d8dbac7f1f2e25eef5d1e29b06a8a8
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-03-02 08:42:18 -0800

    encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,28 @@
-#include "main.h"                                                                                                                     
-                                                                                                                                      
-/**                                                                                                                                   
- * leet - Encodes a string to 1337.                                                                                                   
- * @str: The string to be encoded.                                                                                                    
- *                                                                                                                                    
- * Return: A pointer to the encoded string.                                                                                           
- */                                                                                                                                   
-char *leet(char *str)
+#include "main.h"
+
+/**
+ *  leet - encode into 1337speak
+ *  @n: input value
+ *  Return: n value
+ */
+
+char *leet(char *n)
 {
-	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
 
-	while (str[indx1])
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[indx1] == leet[indx2] ||
-			str[indx1] - 32 == leet[indx2])
-			str[indx1] = indx2 + '0';
-		}
-		
-		indx1++;
-	}
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
 
-	return (str);                                                                                                                 
+		}
+	}
+	return (n);
 }
