309756ecb0a3a562bed2b5d18a5a1ee2b83412f9
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-03-02 08:10:13 -0800

    Copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int j;
 
-	while (src[index++])
+	j = 0;
+
+	while (j < n && src[j] != '\0')
 	{
-		src_len++;
-	}
-	for (index = 0; src[index] && index < n; index++)
-	{
-		dest[index] = src[index];
+		dest[j] = src[j];
+		j++;
 	}
 	return (dest);
 }
