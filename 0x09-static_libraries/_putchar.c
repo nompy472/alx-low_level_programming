64ec57b8abc2c52aafdc33a03efbb49812aec781
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-03-10 08:05:49 -0800

    Static Library

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - function
+ * @c - printed out
+ * Return: c
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
