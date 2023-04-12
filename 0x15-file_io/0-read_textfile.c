928c20188cee1d64f3b46f33355443c2b197b41a
Siphiwe Chiloane <nompychiloane@gmail.com>
2023-04-12 09:17:04 -0700

    reads a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,14 +16,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(filename, O_RONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(size(char) * letters);
+	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
-	return(w);
+	return (w);
 }
