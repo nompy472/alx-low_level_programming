929ac61cce76422cccc37c6860ecc72b6cf643b5
Siphiwe <nompychiloane@gmail.com>
2023-06-19 12:49:58 -0700

    dynamic library that contains C functions

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - sums a and b
+ * @a: variable
+ * @b: variable
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substracts b from a
+ * @a: number
+ * @b: number
+ * Return: difference
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - miltiplies a and b
+ * @a: number used
+ * @b: number used
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - devides a and b
+ * @a: no.
+ * @b: no.
+ * Return: quotient
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - checks remainder
+ * @a: number
+ * @b: number
+ * Return: remainder
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
